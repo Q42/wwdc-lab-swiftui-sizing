3b420d851f6444d61d79637c687fb9f667b2dc16
Mathijs Bernson <mathijsb@q42.nl>
2023-05-19 11:58:54 +0200

    Initial commit

---
+++ SwiftUISizing/SwiftUISizingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISizingApp.swift
+//  SwiftUISizing
+//
+//  Created by Mathijs Bernson on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISizingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
